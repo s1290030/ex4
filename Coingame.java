f485692a3aa6d2b92374ce820599845e68da82ed
KOAKUTSU Yoshiki <s1290030@sshsv212.u-aizu.ac.jp>
2022-08-03 00:59:30 +0900

    User name added

---
+++ Coingame.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Coingame {
+    public static void main(String[] args) {
+    System.out.println("Who are you?");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.println("Hello, " + name + "!");
+}
+}
