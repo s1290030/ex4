2fde3a050ff580661c8b94c1adfbd2d071fa0e65
KOAKUTSU Yoshiki <s1290030@sshsv212.u-aizu.ac.jp>
2022-08-03 01:03:18 +0900

    Victory message added

--- CoinGame.java
+++ CoinGame.java
@@ -19,5 +19,11 @@ public class CoinGame {
             }
         }
         System.out.println("heads: " + heads + ", Tails: " + tails);
+
+if (heads > tails) {
+            System.out.println("You won!");
+        } else {
+    System.out.println( "You lose!");
+        }
     }
 }
