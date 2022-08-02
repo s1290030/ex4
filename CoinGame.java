b67ce09668ef6616db6f03da5adbf0adae4e1d31
KOAKUTSU Yoshiki <s1290030@sshsv212.u-aizu.ac.jp>
2022-08-03 00:52:23 +0900

    HoT: initial revision

---
+++ CoinGame.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class CoinGame {
+    public static void main(String[] args) {
+        double[] coin = new double[3];
+        int heads = 0;
+        int tails = 0;
+	System.out.println("Tossing a coin...");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Rounds " + (i + 1) + ": ");
+            coin[i] = Math.floor(Math.random() * 2) + 1;
+
+            if (coin[i] == 1) {
+                System.out.println("Heads");
+                heads++;
+            } else {
+                System.out.println("Tails");
+                tails++;
+            }
+        }
+        System.out.println("heads: " + heads + ", Tails: " + tails);
+    }
+}
